597c5fb54c63fb0c9e262431c333529e4a64746c
Bonnie Simon <bonnie.developer@gmail.com>
2023-12-02 14:13:14 +0530

    Add simpler solution without stacks

--- c/day01/main.c
+++ c/day01/main.c
@@ -10,20 +10,14 @@ PART 1
 #include <ctype.h>
 #include <stdbool.h>
 
-typedef struct stack{
-  int max;
-  int top;
-  int *items;
-} Stack;
-
-Stack *init_stack(int len);
-void push(int value, Stack *s);
-void print_stack(Stack *s);
-int pop(Stack *s);
 int calculate_magic_number(char *word, size_t length);
 
-int main() {
-  FILE *file = fopen("numberset.txt", "r");
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("File not passed as argument to executable\n");
+  }
+
+  FILE *file = fopen(argv[1], "r");
 
   if (file == NULL) {
     printf("File cannot be opened\n");
@@ -47,73 +41,22 @@ int main() {
   return 0;
 }
 
-Stack *init_stack(int len) {
-  Stack *s = (Stack *) malloc(sizeof(Stack));
-  (*s).max = len;
-  (*s).top = -1;
-  (*s).items = (int *) malloc(sizeof(int));
-
-  return s;
-}
-
-void push(int value, Stack *s) {
-  if (s->top == s->max - 1) {
-    printf("Stackoverflow");
-    return;
-  }
-
-  s->top++;
-  s->items[s->top] = value;
-}
-
-void print_stack(Stack *s) {
-  if (s->top == -1) {
-    printf("Stack empty\n");
-    return;
-  }
-
-  for (int i = 0; i <= s->top; i++) {
-    printf("%d ---", s->items[i]);
-  }
-}
-
-int pop(Stack *s) {
-  if (s->top == -1) {
-    return -1;
-  }
-
-  int value = s->items[s->top];
-  s->top--;
-
-  return value;
-}
 
 int calculate_magic_number(char *word, size_t length) {
-  Stack *nums = init_stack(length);
+
+  int first = -1;
+  int last = -1;
 
   for (size_t i = 0; i < length; i++) {
     if (word[i] < '0' || word[i] > '9') continue;
 
-    push(word[i] - '0', nums);
+    if (first == -1) {
+      first = word[i] - '0';
+      last = word[i] - '0';
+    } else {
+      last = word[i] - '0';
+    }
   }
-
-
-  int last = pop(nums);
-  int first = pop(nums);
-
-  if (first == -1) {
-    first = last;
-  }
-
-  int next_number = pop(nums);
-
-  while(next_number != -1) {
-    first = next_number;
-
-    next_number = pop(nums);
-  }
-
-  free(nums);
 
   return first * 10 + last;
 }
