04b93767221e41b7c22d5d1c128273ce6179d09c
Bonnie Simon <bonnie.developer@gmail.com>
2023-12-02 13:03:56 +0530

    Solved day01 part 1

--- day01/main.c
+++ day01/main.c
@@ -4,52 +4,108 @@
 #include <ctype.h>
 #include <stdbool.h>
 
+typedef struct stack{
+  int max;
+  int top;
+  int *items;
+} Stack;
+
+Stack *init_stack(int len);
+void push(int value, Stack *s);
+void print_stack(Stack *s);
+int pop(Stack *s);
+int calculate_magic_number(char *word, size_t length);
+
 int main() {
-  char question[] = "a1b2c3d4e5f";
-  char *digits = (char *) malloc(sizeof(char) * sizeof(question));
-  char *pos_digits = digits;
-  char *first, *last;
+  FILE *file = fopen("numberset.txt", "r");
 
-  first = digits;
-  last = digits;
-
-  bool atleast_one = false;
-
-  printf("%c \n", *digits);
-  while( *digits != '\0') {
-    if (*digits < '0' || *digits > '9') continue;
-
-    printf("%c \n", *digits);
-
-    if (!atleast_one) {
-      first = digits;
-      last = digits;
-      printf("%s", first);
-      atleast_one = true;
-      digits++;
-      break;
-    }
-
-    last = digits;
-
-    digits++;
+  if (file == NULL) {
+    printf("File cannot be opened\n");
+    return 0;
   }
 
-  printf("%s", last);
-  /*
-  for(size_t i = 0; question[i] != '\0'; i++) {
-    if(question[i] >= '0' && question[i] <= '9'){
-      *pos_digits = question[i];
-      pos_digits++;
-    }
+  char word[100];
+
+  int sum = 0;
+
+  while(fscanf(file, "%s", word) == 1)  {
+    size_t length = strlen(word);
+
+    sum += calculate_magic_number(word, length);
   }
 
-  for (size_t i = 0; digits[i] != '\0'; i++) {
-    printf("%s\n", digits[i]);
-  }
-  */
+  printf("%d\n", sum);
 
-
-  free(digits);
   return 0;
+}
+
+Stack *init_stack(int len) {
+  Stack *s = (Stack *) malloc(sizeof(Stack));
+  (*s).max = len;
+  (*s).top = -1;
+  (*s).items = (int *) malloc(sizeof(int));
+
+  return s;
+}
+
+void push(int value, Stack *s) {
+  if (s->top == s->max - 1) {
+    printf("Stackoverflow");
+    return;
+  }
+
+  s->top++;
+  s->items[s->top] = value;
+}
+
+void print_stack(Stack *s) {
+  if (s->top == -1) {
+    printf("Stack empty\n");
+    return;
+  }
+
+  for (int i = 0; i <= s->top; i++) {
+    printf("%d ---", s->items[i]);
+  }
+}
+
+int pop(Stack *s) {
+  if (s->top == -1) {
+    return -1;
+  }
+
+  int value = s->items[s->top];
+  s->top--;
+
+  return value;
+}
+
+int calculate_magic_number(char *word, size_t length) {
+  Stack *nums = init_stack(length);
+
+  for (size_t i = 0; i < length; i++) {
+    if (word[i] < '0' || word[i] > '9') continue;
+
+    push(word[i] - '0', nums);
+  }
+
+
+  int last = pop(nums);
+  int first = pop(nums);
+
+  if (first == -1) {
+    first = last;
+  }
+
+  int next_number = pop(nums);
+
+  while(next_number != -1) {
+    first = next_number;
+
+    next_number = pop(nums);
+  }
+
+  free(nums);
+
+  return first * 10 + last;
 }
